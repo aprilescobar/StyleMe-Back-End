b7df3109b53061dc43ea5efcbda4beb57e1eb915
aprilfaithdays <aprilfaith.e@gmail.com>
2020-05-17 19:48:08 -0400

    added CD methods to tops, bottoms, and shoes controllers

--- app/controllers/favorite_bottoms_controller.rb
+++ app/controllers/favorite_bottoms_controller.rb
@@ -2,5 +2,20 @@ class FavoriteBottomsController < ApplicationController
     def index
         favoriteBottoms = FavoriteBottom.all
         render json: favoriteBottoms, except: [:updated_at]
+     end
+     
+     def create
+        favorite_bottom = FavoriteBottom.create(favorite_bottom_params)
+        render json: favorite_bottom, except: [:updated_at], include: [:bottom], status:201
      end 
+
+     def destroy 
+        favorite_bottom = FavoriteBottom.find_by(id: params[:id])
+        favorite_bottom.destroy
+     end 
+  
+     private 
+     def favorite_bottom_params
+        params.require(:favorite_bottom).permit(:user_id, :bottom_id)
+     end    
 end
