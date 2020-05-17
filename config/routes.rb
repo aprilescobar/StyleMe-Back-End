c73529f6d6a28c22e113950a6ed07c567b412f21
aprilfaithdays <aprilfaith.e@gmail.com>
2020-05-16 21:42:23 -0400

    routes updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  resources :favorite_shoes
-  resources :favorite_bottoms
-  resources :favorite_tops
+  resources :favorite_shoes, only: [:index]
+  resources :favorite_bottoms , only: [:index]
+  resources :favorite_tops , only: [:index]
   resources :outfits
-  resources :shoes
-  resources :bottoms
-  resources :tops
+  resources :shoes, only: [:index, :show]
+  resources :bottoms, only: [:index, :show]
+  resources :tops, only: [:index, :show]
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
